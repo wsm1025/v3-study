b0d6800f4318cc12de9d1cdb7618ad6ace8aa595
wsm1025 <taizongwang@126.com>
2023-05-09 23:13:28 +0800

    ref 复杂类型完善

--- ref.ts
+++ ref.ts
@@ -1,26 +1,32 @@
 import { isTracking, trackEffects, triggerEffects } from "./effect";
-import { hasChange } from "./share";
+import { reactive } from "./reactive";
+import { hasChange, isObject } from "./share";
 class RefImpl {
   private _value;
-  public dep;
+  private dep;
+  private _rawValue: any;
   constructor(value: any) {
-    this._value = value;
+    this._rawValue = value;
+    this._value = convert(value);
     this.dep = new Set();
   }
   get value() {
     trackRefValue(this);
     return this._value;
   }
-  set value(value) {
+  set value(newValue) {
     // 值改变了才会 去收集依赖
-    if (hasChange(value, this._value)) {
+    if (hasChange(newValue, this._rawValue)) {
       // 先去修改 value
-      this._value = value;
+      this._rawValue = newValue;
+      this._value = convert(newValue);
       triggerEffects(this.dep);
     }
   }
 }
-
+function convert(value) {
+  return isObject(value) ? reactive(value) : value;
+}
 function trackRefValue(ref) {
   if (isTracking()) {
     trackEffects(ref.dep);
