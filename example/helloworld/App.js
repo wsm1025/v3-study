07179dfe7c2be20cd390433ae550bcde05ff149a
wsm1025 <taizongwang@126.com>
2023-05-14 23:08:11 +0800

    组件与元素的挂载

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,7 +1,11 @@
 import { h } from "../../lib/myVue.esm.js";
 export const App = {
   render() {
-    return h("div", "hi" + this.message);
+    // return h("div", { id: "root", class: ["red weight"] }, "hi," + "my vue");
+    return h("div", { id: "root", class: ["red weight"] }, [
+      h("div", null, "hi"),
+      h("p", { class: ["blue"] }, h("span", null, "niubi")),
+    ]);
   },
   setup() {
     return {
--- runtime-core/renderer.ts
+++ runtime-core/renderer.ts
@@ -1,13 +1,18 @@
+import { isObject } from "../share/index";
 import { createComponentInstance, setupComponent } from "./componets";
 
 export function render(vnode, container) {
   patch(vnode, container);
 }
 function patch(vnode, container) {
+  debugger;
   // 处理组件
-  processComponent(vnode, container);
   // TODO 判断 vnode 是不是 elemnet
-  // processElement()
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
 }
 function processComponent(vnode, container) {
   mountComponent(vnode, container);
@@ -22,4 +27,36 @@ function setupRenderEffect(instance: any, container: any) {
   // 虚拟节点树🌲
   const subTree = instance.render();
   patch(subTree, container);
+}
+function processElement(vnode: any, container: any) {
+  // init
+  mountElement(vnode, container);
+  // update
+}
+function mountElement(vnode: any, container: any) {
+  let { type, props, children } = vnode;
+  // type
+  const el = document.createElement(type);
+  // 内容
+  console.log(children);
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(children, container);
+  } else if (isObject(children)) {
+    patch(children, el);
+  }
+  // props
+  if (props) {
+    for (const key in props) {
+      el.setAttribute(key, props[key]);
+    }
+  }
+  container.appendChild(el);
+
+  function mountChildren(children: Array<Element>, el) {
+    children.forEach((v) => {
+      patch(v, el);
+    });
+  }
 }
